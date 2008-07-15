85b2059354c5cb53dae32b675d988079391a312a
Huang Peng <shawn.p.huang@gmail.com>
2008-07-15 17:05:53 +0800

    Change vim settings and replace tab with spaces.

--- engine/factory.py
+++ engine/factory.py
@@ -1,4 +1,4 @@
-# vim:set et ts=4:
+# vim:set et sts=4 sw=4:
 #
 # ibus-tmpl - The Input Bus template project
 #
@@ -32,7 +32,7 @@ class EngineFactory(interface.IEngineFactory):
     CREDITS = "GPLv2"
 
     def __init__(self, dbusconn):
-		super(EngineFactory, self).__init__(dbusconn, object_path = FACTORY_PATH)
+        super(EngineFactory, self).__init__(dbusconn, object_path = FACTORY_PATH)
         self._dbusconn = dbusconn
         self._max_engine_id = 1
 
