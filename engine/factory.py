41fd717f3b26c1e337e3d92f78f67c9a79926818
Huang Peng <shawn.p.huang@gmail.com>
2008-07-15 13:42:15 +0800

    Refine coding style

--- engine/factory.py
+++ engine/factory.py
@@ -1,4 +1,4 @@
-# vim:set noet ts=4:
+# vim:set et ts=4:
 #
 # ibus-tmpl - The Input Bus template project
 #
@@ -24,34 +24,34 @@ import engine
 FACTORY_PATH = "/com/redhat/IBus/engines/Demo/Factory"
 ENGINE_PATH = "/com/redhat/IBus/engines/Demo/Engine/%d"
 
-class DemoEngineFactory (interface.IEngineFactory):
-	NAME = "Enchant"
-	LANG = "en"
-	ICON = "ibus-enchant"
-	AUTHORS = "Huang Peng <shawn.p.huang@gmail.com>"
-	CREDITS = "GPLv2"
+class EngineFactory(interface.IEngineFactory):
+    NAME = "Enchant"
+    LANG = "en"
+    ICON = "ibus-enchant"
+    AUTHORS = "Huang Peng <shawn.p.huang@gmail.com>"
+    CREDITS = "GPLv2"
 
-	def __init__ (self, dbusconn):
-		interface.IEngineFactory.__init__ (self, dbusconn, object_path = FACTORY_PATH)
-		self._dbusconn = dbusconn
-		self._max_engine_id = 1
-	
-	def GetInfo (self):
-		return [
-			self.NAME,
-			self.LANG,
-			self.ICON,
-			self.AUTHORS,
-			self.CREDITS
-			]
+    def __init__(self, dbusconn):
+		super(EngineFactory, self).__init__(dbusconn, object_path = FACTORY_PATH)
+        self._dbusconn = dbusconn
+        self._max_engine_id = 1
 
-	def CreateEngine (self):
-		engine_path = ENGINE_PATH % self._max_engine_id
-		self._max_engine_id += 1
-		return engine.DemoEngine (self._dbusconn, engine_path)
+    def GetInfo(self):
+        return [
+            self.NAME,
+            self.LANG,
+            self.ICON,
+            self.AUTHORS,
+            self.CREDITS
+            ]
 
-	def Destroy (self):
-		self.remove_from_connection ()
-		self._dbusconn = None
+    def CreateEngine(self):
+        engine_path = ENGINE_PATH % self._max_engine_id
+        self._max_engine_id += 1
+        return engine.DemoEngine(self._dbusconn, engine_path)
+
+    def Destroy(self):
+        self.remove_from_connection()
+        self._dbusconn = None
 
 
