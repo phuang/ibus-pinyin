efddab95f00292f4ffe69fbcc71055c4d1abbdb3
Huang Peng <shawn.p.huang@gmail.com>
2008-07-16 19:43:10 +0800

    Replace tap with spaces.

--- engine/pyparser.py
+++ engine/pyparser.py
@@ -24,130 +24,130 @@ import pyutil
 import pydict
 
 class PinYinParser:
-	pinyin_dict = set (pydict.PINYIN_DICT.keys () + pydict.SHENGMU_DICT.keys ())
-	gb2312_pinyin_dict = pinyin_dict - set (["eng", "chua", "fe", "fiao", "liong"])
-	correct_yunmu = { 
-		"ing" : ("ign", "img"), "ui" : ("uei", "iu", "i"), 
-		"un" : ("uen",), "iu" : ("iou", "ui"),
-		"ao" : ("au", ), "ei" : ("i", ),
-		"iao" : ("ioa", "ia", "i"), "ian" : ("ia", "i"), "iang" : ("ian", "ia", "i")}
+    pinyin_dict = set (pydict.PINYIN_DICT.keys () + pydict.SHENGMU_DICT.keys ())
+    gb2312_pinyin_dict = pinyin_dict - set (["eng", "chua", "fe", "fiao", "liong"])
+    correct_yunmu = { 
+        "ing" : ("ign", "img"), "ui" : ("uei", "iu", "i"), 
+        "un" : ("uen",), "iu" : ("iou", "ui"),
+        "ao" : ("au", ), "ei" : ("i", ),
+        "iao" : ("ioa", "ia", "i"), "ian" : ("ia", "i"), "iang" : ("ian", "ia", "i")}
 
-	correct_table = {}
-	
-	def __init__ (self):
-		self.init_corrent_table ()
+    correct_table = {}
+    
+    def __init__ (self):
+        self.init_corrent_table ()
 
-	def init_corrent_table (self):
-		if PinYinParser.correct_table:
-			return
-		for key, id in pydict.PINYIN_DICT.items ():
-			if key[-3:] in PinYinParser.correct_yunmu:
-				for yunmu in PinYinParser.correct_yunmu[key[-3:]]:
-					pinyin = key[:-3] + yunmu
-					if pinyin not in pydict.PINYIN_DICT:
-						PinYinParser.correct_table [pinyin] = key
-			if key[-2:] in PinYinParser.correct_yunmu:
-				for yunmu in PinYinParser.correct_yunmu[key[-2:]]:
-					pinyin = key[:-2] + yunmu
-					if pinyin not in pydict.PINYIN_DICT:
-						PinYinParser.correct_table [pinyin] = key
+    def init_corrent_table (self):
+        if PinYinParser.correct_table:
+            return
+        for key, id in pydict.PINYIN_DICT.items ():
+            if key[-3:] in PinYinParser.correct_yunmu:
+                for yunmu in PinYinParser.correct_yunmu[key[-3:]]:
+                    pinyin = key[:-3] + yunmu
+                    if pinyin not in pydict.PINYIN_DICT:
+                        PinYinParser.correct_table [pinyin] = key
+            if key[-2:] in PinYinParser.correct_yunmu:
+                for yunmu in PinYinParser.correct_yunmu[key[-2:]]:
+                    pinyin = key[:-2] + yunmu
+                    if pinyin not in pydict.PINYIN_DICT:
+                        PinYinParser.correct_table [pinyin] = key
 
-	def parse_recursive (self, string, auto_correct = True, gbk = True):
-		l = min (6, len (string))
-		if l == 0:
-			return []
-		p = None
-		for i in range (l, 0, -1):
-			py = string[-i:]
-			
-			if gbk:
-				if py in self.pinyin_dict:
-					p = pyutil.PinYinWord (py)
-					break
-			else:
-				if py in self.gb2312_pinyin_dict:
-					p = pyutil.PinYinWord (py)
-					break
-			
-			if p == None and auto_correct and py in PinYinParser.correct_table:
-				py = PinYinParser.correct_table[py]
-				if gbk:
-					if py in self.pinyin_dict:
-						p = pyutil.PinYinWord (py)
-						break
-				else:
-					if py in self.gb2312_pinyin_dict:
-						p = pyutil.PinYinWord (py)
-						break
-		if p == None:
-			raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
-		pys = self.parse_recursive (string[:-i], auto_correct, gbk)
-		pys.append (p)
-		return pys
+    def parse_recursive (self, string, auto_correct = True, gbk = True):
+        l = min (6, len (string))
+        if l == 0:
+            return []
+        p = None
+        for i in range (l, 0, -1):
+            py = string[-i:]
+            
+            if gbk:
+                if py in self.pinyin_dict:
+                    p = pyutil.PinYinWord (py)
+                    break
+            else:
+                if py in self.gb2312_pinyin_dict:
+                    p = pyutil.PinYinWord (py)
+                    break
+            
+            if p == None and auto_correct and py in PinYinParser.correct_table:
+                py = PinYinParser.correct_table[py]
+                if gbk:
+                    if py in self.pinyin_dict:
+                        p = pyutil.PinYinWord (py)
+                        break
+                else:
+                    if py in self.gb2312_pinyin_dict:
+                        p = pyutil.PinYinWord (py)
+                        break
+        if p == None:
+            raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
+        pys = self.parse_recursive (string[:-i], auto_correct, gbk)
+        pys.append (p)
+        return pys
 
-	def parse (self, string, auto_correct = True, gbk = True):
-		try:
-			pys = []
-			for py in string.split (u"'"):
-				pys += self.parse_recursive (py, auto_correct, gbk)
-			return pys
-		except Exception, e:
-			import traceback
-			traceback.print_exc ()
-			raise e
+    def parse (self, string, auto_correct = True, gbk = True):
+        try:
+            pys = []
+            for py in string.split (u"'"):
+                pys += self.parse_recursive (py, auto_correct, gbk)
+            return pys
+        except Exception, e:
+            import traceback
+            traceback.print_exc ()
+            raise e
 
 class ShuangPinParser:
-	def __init__ (self, schema = "MSPY"):
-		self._gbk = True
-		self._schema = schema
-		self._shengmu_dict, self._yunmu_dict = pydict.SHUANGPIN_SCHEMAS[self._schema]
+    def __init__ (self, schema = "MSPY"):
+        self._gbk = True
+        self._schema = schema
+        self._shengmu_dict, self._yunmu_dict = pydict.SHUANGPIN_SCHEMAS[self._schema]
 
-	def parse_shuangpin_recursive (self, pys, string, auto_correct = True, gbk = True):
-		if len (string) == 0:
-			return []
+    def parse_shuangpin_recursive (self, pys, string, auto_correct = True, gbk = True):
+        if len (string) == 0:
+            return []
 
-		if len (string) == 1:
-			try:
-				shengmu = self._shengmu_dict[string[0]]
-				if shengmu == "'":
-					shengmu = ""
-			except:
-				raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
-			
-			return [pyutil.PinYinWord (shengmu)]
+        if len (string) == 1:
+            try:
+                shengmu = self._shengmu_dict[string[0]]
+                if shengmu == "'":
+                    shengmu = ""
+            except:
+                raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
+            
+            return [pyutil.PinYinWord (shengmu)]
 
-		try:
-			shengmu = self._shengmu_dict[string[0]]
-			if shengmu == "'":
-				shengmu = ""
-			yunmu = self._yunmu_dict[string[1]]
-		except:
-			raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
+        try:
+            shengmu = self._shengmu_dict[string[0]]
+            if shengmu == "'":
+                shengmu = ""
+            yunmu = self._yunmu_dict[string[1]]
+        except:
+            raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
 
-		p = None
+        p = None
 
-		for i in yunmu:
-			pinyin = shengmu + i
-			if pinyin in PinYinParser.pinyin_dict:
-				p = pyutil.PinYinWord (pinyin)
-				break
-		
-		if p == None:
-			raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
+        for i in yunmu:
+            pinyin = shengmu + i
+            if pinyin in PinYinParser.pinyin_dict:
+                p = pyutil.PinYinWord (pinyin)
+                break
+        
+        if p == None:
+            raise Exception ("can not parse '%s'" % string.encode ("utf-8"))
 
-		pys.append (p)
-		pys = self.parse_shuangpin_recursive (pys, string[2:], auto_correct, gbk)
+        pys.append (p)
+        pys = self.parse_shuangpin_recursive (pys, string[2:], auto_correct, gbk)
 
-		return pys
+        return pys
 
-	def parse (self, string, auto_correct = True, gbk = True):
-		pys = []
-		pys += self.parse_shuangpin_recursive (pys, string, auto_correct, gbk)
-		return pys
+    def parse (self, string, auto_correct = True, gbk = True):
+        pys = []
+        pys += self.parse_shuangpin_recursive (pys, string, auto_correct, gbk)
+        return pys
 
 if __name__ == "__main__":
-	# parser = PinYinParser ()
-	parser = ShuangPinParser ()	
-	pys = parser.parse (sys.argv[1])
-	print "'".join (map (str, pys))
-				
+    # parser = PinYinParser ()
+    parser = ShuangPinParser ()    
+    pys = parser.parse (sys.argv[1])
+    print "'".join (map (str, pys))
+                
--- engine/pyutil.py
+++ engine/pyutil.py
@@ -22,127 +22,127 @@
 from pydict import *
 
 class PinYinWord:
-	correct_dict = {"nve" : "nue", "lve" : "lue"}
-	def __init__ (self, pinyin):
-		if pinyin in self.correct_dict:
-			pinyin = self.correct_dict [pinyin]
+    correct_dict = {"nve" : "nue", "lve" : "lue"}
+    def __init__ (self, pinyin):
+        if pinyin in self.correct_dict:
+            pinyin = self.correct_dict [pinyin]
 
-		self._pinyin = pinyin
-		self._is_completed = self.is_valid_pinyin ()
-		if self._is_completed:
-			sheng_mu, yun_mu = self.split ()
-			self._pinyin_id = PINYIN_DICT [self._pinyin]
-			self._sheng_mu_id = SHENGMU_DICT [sheng_mu]
-		else:
-			self._sheng_mu_id = SHENGMU_DICT [self._pinyin]
-	
-	def is_valid_pinyin (self):
-		return PINYIN_DICT.has_key (self._pinyin)
+        self._pinyin = pinyin
+        self._is_completed = self.is_valid_pinyin ()
+        if self._is_completed:
+            sheng_mu, yun_mu = self.split ()
+            self._pinyin_id = PINYIN_DICT [self._pinyin]
+            self._sheng_mu_id = SHENGMU_DICT [sheng_mu]
+        else:
+            self._sheng_mu_id = SHENGMU_DICT [self._pinyin]
+    
+    def is_valid_pinyin (self):
+        return PINYIN_DICT.has_key (self._pinyin)
 
-	def get_sheng_mu_id (self):
-		return self._sheng_mu_id
+    def get_sheng_mu_id (self):
+        return self._sheng_mu_id
 
-	def get_shengmu (self):
-		return ID_SHENGMU_DICT[self._sheng_mu_id]
+    def get_shengmu (self):
+        return ID_SHENGMU_DICT[self._sheng_mu_id]
 
-	def get_pinyin_id (self):
-		return self._pinyin_id
+    def get_pinyin_id (self):
+        return self._pinyin_id
 
-	def get_pinyin (self):
-		return self._pinyin
+    def get_pinyin (self):
+        return self._pinyin
 
-	def get_pattern (self, mohu = False):
-		if mohu == False:
-			if self.is_valid_pinyin ():
-				return self._pinyin
-			else:
-				return self._pinyin + "%"
-		else:
-			if not self.is_valid_pinyin ():
-				if self._pinyin in ("zh", "ch", "sh"):
-					return self._pinyin[0] + "%"
-				return self._pinyin + "%"
-			else:
-				shengmu = self.get_shengmu ()
-				yunmu = self._pinyin [len (shengmu):]
-				if shengmu in ("zh", "ch", "sh", "z", "c", "s"):
-					shengmu = shengmu[0] + "%"
-				if yunmu in ("ing", "in", "en", "eng", "an", "ang"):
-					yunmu = yunmu[0:2] + "%"
-				return shengmu + yunmu
+    def get_pattern (self, mohu = False):
+        if mohu == False:
+            if self.is_valid_pinyin ():
+                return self._pinyin
+            else:
+                return self._pinyin + "%"
+        else:
+            if not self.is_valid_pinyin ():
+                if self._pinyin in ("zh", "ch", "sh"):
+                    return self._pinyin[0] + "%"
+                return self._pinyin + "%"
+            else:
+                shengmu = self.get_shengmu ()
+                yunmu = self._pinyin [len (shengmu):]
+                if shengmu in ("zh", "ch", "sh", "z", "c", "s"):
+                    shengmu = shengmu[0] + "%"
+                if yunmu in ("ing", "in", "en", "eng", "an", "ang"):
+                    yunmu = yunmu[0:2] + "%"
+                return shengmu + yunmu
 
-	def split (self):
-		if not self.is_valid_pinyin ():
-			raise Exception ("Pinyin '%s' is not a valid pinyin!" % py)
-		if self._pinyin[:2] in SHENGMU_DICT.keys ():
-			return self._pinyin[:2], self._pinyin[2:]
-		elif self._pinyin[:1] in SHENGMU_DICT.keys ():
-			return self._pinyin[:1], self._pinyin[1:]
-		else:
-			return "", self._pinyin[:]
+    def split (self):
+        if not self.is_valid_pinyin ():
+            raise Exception ("Pinyin '%s' is not a valid pinyin!" % py)
+        if self._pinyin[:2] in SHENGMU_DICT.keys ():
+            return self._pinyin[:2], self._pinyin[2:]
+        elif self._pinyin[:1] in SHENGMU_DICT.keys ():
+            return self._pinyin[:1], self._pinyin[1:]
+        else:
+            return "", self._pinyin[:]
 
-	def __str__ (self):
-		return self._pinyin
+    def __str__ (self):
+        return self._pinyin
 
 class PinYinString:
-	def __init__ (self, string):
-		pass
+    def __init__ (self, string):
+        pass
 
 def load_pinyin_table (_file):
-	
-	def pinyin_table_parser (f):
-		for l in f:
-			a = unicode (l, "utf-8").strip ().split ()
-			hanzi, pinyin, freq = a 
-			yield (hanzi, pinyin, int (freq))
-	# db.add_phrases (pinyin_table_parser (bzf))
-	
-	hanzi_dic = {}
-	for hanzi, pinyin, freq in pinyin_table_parser (_file):
-		if not hanzi_dic.has_key (hanzi):
-			hanzi_dic[hanzi] = {}
-		
-		if hanzi_dic[hanzi].has_key (pinyin):
-			hanzi_dic[hanzi][pinyin] += freq
-		else:
-			hanzi_dic[hanzi][pinyin] = freq
+    
+    def pinyin_table_parser (f):
+        for l in f:
+            a = unicode (l, "utf-8").strip ().split ()
+            hanzi, pinyin, freq = a 
+            yield (hanzi, pinyin, int (freq))
+    # db.add_phrases (pinyin_table_parser (bzf))
+    
+    hanzi_dic = {}
+    for hanzi, pinyin, freq in pinyin_table_parser (_file):
+        if not hanzi_dic.has_key (hanzi):
+            hanzi_dic[hanzi] = {}
+        
+        if hanzi_dic[hanzi].has_key (pinyin):
+            hanzi_dic[hanzi][pinyin] += freq
+        else:
+            hanzi_dic[hanzi][pinyin] = freq
 
-	return hanzi_dic
+    return hanzi_dic
 
 def load_phrase_pinyin_freq (_file):
-	def phrase_pinyin_parser (f):
-		for l in f:
-			phrase, pinyin, freq = unicode (l, "utf-8").strip ().split ()
-			pinyin = pinyin.replace (u"u:", u"v")
-			yield (phrase, pinyin, int (freq))
-	phrases_dic = {}
-	for phrase, pinyin, freq in phrase_pinyin_parser (_file):
-		if not phrases_dic.has_key (phrase):
-			phrases_dic[phrase] = []
-		phrases_dic[phrase].append ((phrase, pinyin, freq))
+    def phrase_pinyin_parser (f):
+        for l in f:
+            phrase, pinyin, freq = unicode (l, "utf-8").strip ().split ()
+            pinyin = pinyin.replace (u"u:", u"v")
+            yield (phrase, pinyin, int (freq))
+    phrases_dic = {}
+    for phrase, pinyin, freq in phrase_pinyin_parser (_file):
+        if not phrases_dic.has_key (phrase):
+            phrases_dic[phrase] = []
+        phrases_dic[phrase].append ((phrase, pinyin, freq))
 
-	return phrases_dic
+    return phrases_dic
 
 def load_phrase_pinyin (_file):
-	def phrase_pinyin_parser (f):
-		for l in f:
-			phrase, pinyin = unicode (l, "utf-8").strip ().split ()
-			pinyin = pinyin.replace (u"u:", u"v")
-			yield (phrase, pinyin, 0)
-	phrases_dic = {}
-	for phrase, pinyin, freq in phrase_pinyin_parser (_file):
-		if not phrases_dic.has_key (phrase):
-			phrases_dic[phrase] = []
-		phrases_dic[phrase].append ((phrase, pinyin, freq))
+    def phrase_pinyin_parser (f):
+        for l in f:
+            phrase, pinyin = unicode (l, "utf-8").strip ().split ()
+            pinyin = pinyin.replace (u"u:", u"v")
+            yield (phrase, pinyin, 0)
+    phrases_dic = {}
+    for phrase, pinyin, freq in phrase_pinyin_parser (_file):
+        if not phrases_dic.has_key (phrase):
+            phrases_dic[phrase] = []
+        phrases_dic[phrase].append ((phrase, pinyin, freq))
 
-	return phrases_dic
+    return phrases_dic
 
 def load_sogou_phrases (_file):
-	import re
-	dic = {}
-	for l in _file:
-		w = unicode (l, "utf8")
-		w = re.split (ur"\t+", w)
-		dic [w[0]] = (w[0], int (w[1]))
-	return dic
+    import re
+    dic = {}
+    for l in _file:
+        w = unicode (l, "utf8")
+        w = re.split (ur"\t+", w)
+        dic [w[0]] = (w[0], int (w[1]))
+    return dic
 
