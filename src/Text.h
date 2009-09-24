64cc8d82fb927baa0d0fdd14bfd0a295b9ff22c0
Peng Huang <shawn.p.huang@gmail.com>
2009-09-24 15:48:09 +0800

    Refine coding style

--- src/Text.h
+++ src/Text.h
@@ -9,15 +9,15 @@ namespace PY {
 class Text : public Pointer<IBusText> {
 public:
     Text (IBusText *text)
-        : Pointer<IBusText> (text) { }
+        : Pointer<IBusText> (text) {}
     Text (const gchar *str)
-        : Pointer<IBusText> (ibus_text_new_from_string (str)) { }
+        : Pointer<IBusText> (ibus_text_new_from_string (str)) {}
 
     Text (const String & str)
-        : Pointer<IBusText> (ibus_text_new_from_string ((const gchar *) str)) { }
+        : Pointer<IBusText> (ibus_text_new_from_string ((const gchar *) str)) {}
 
     Text (gunichar ch)
-        : Pointer<IBusText> (ibus_text_new_from_unichar (ch)) { }
+        : Pointer<IBusText> (ibus_text_new_from_unichar (ch)) {}
 
     void appendAttribute (guint type, guint value, guint start, guint end) {
         ibus_text_append_attribute (*this, type, value, start, end);
@@ -27,14 +27,13 @@ public:
 class StaticText : public Text {
 public:
     StaticText (const gchar *str)
-        : Text (ibus_text_new_from_static_string (str)) { }
+        : Text (ibus_text_new_from_static_string (str)) {}
 
     StaticText (const String & str)
-        : Text (ibus_text_new_from_static_string ((const gchar *) str)) { }
+        : Text (ibus_text_new_from_static_string ((const gchar *) str)) {}
 
     StaticText (gunichar ch)
-        : Text (ch) { }
-
+        : Text (ch) {}
 };
 
 };
