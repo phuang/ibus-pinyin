d8329f5a207efaa020eaa484fd9ec56b7ed75ce2
Peng Huang <shawn.p.huang@gmail.com>
2010-02-12 23:49:01 +0800

    Add ExtEditor.cc

--- src/ExtEditor.h
+++ src/ExtEditor.h
@@ -9,7 +9,18 @@ namespace PY {
 
 class ExtEditor : public Editor {
 public:
-    ExtEditor (PinyinProperties &props) : Editor (props) {}
+    ExtEditor (PinyinProperties &props);
+
+    virtual gboolean processKeyEvent (guint keyval, guint keycode, guint modifiers);
+    virtual void pageUp (void);
+    virtual void pageDown (void);
+    virtual void cursorUp (void);
+    virtual void cursorDown (void);
+    virtual void update (void);
+    virtual void reset (void);
+    virtual void candidateClicked (guint index, guint button, guint state);
+
+private:
 };
 
 };
