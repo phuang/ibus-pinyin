a33abc5b604a056f7c8adb66ce72d3407014d0fd
Peng Huang <shawn.p.huang@gmail.com>
2010-04-20 14:01:56 +0800

    Check boost::signals2 in configure.ac

--- src/Signal.h
+++ src/Signal.h
@@ -1,16 +1,31 @@
 #ifndef __PY_SIGNAL_H_
 #define __PY_SIGNAL_H_
 
-#include <boost/signals2.hpp>
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#ifdef HAVE_BOOST_SIGNALS2_HPP
+#  include <boost/signals2.hpp>
+#elif HAVE_BOOST_SIGNALS_HPP
+#  include <boost/signals.hpp>
+#else
+#  error "Can not find boost.signals2 or boost.signal"
+#endif
+
 #include <boost/bind.hpp>
 
 namespace PY {
 
 using boost::bind;
 
+#if HAVE_BOOST_SIGNALS2_HPP
 template <typename Signature>
 struct signal : public boost::signals2::signal_type <Signature, boost::signals2::keywords::mutex_type<boost::signals2::dummy_mutex> >::type {
-};
+    };
+#elif HAVE_BOOST_SIGNALS_HPP
+using boost::signal;
+#endif
 
 };
 
