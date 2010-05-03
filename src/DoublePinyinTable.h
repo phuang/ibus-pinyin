26b0f41caafe71d918d4c77a3f68fd31afd9436c
Peng Huang <shawn.p.huang@gmail.com>
2010-05-03 14:24:24 +0800

    Add MS double pinyin back

--- src/DoublePinyinTable.h
+++ src/DoublePinyinTable.h
@@ -303,6 +303,7 @@ static const struct {
     const gint  (&sheng)[27];
     const gint  (&yun)[27][2];
 } double_pinyin_map [] = {
+    { double_pinyin_mspy_sheng, double_pinyin_mspy_yun},
     { double_pinyin_zrm_sheng, double_pinyin_zrm_yun},
     { double_pinyin_abc_sheng, double_pinyin_abc_yun},
     { double_pinyin_zgpy_sheng, double_pinyin_zgpy_yun},
