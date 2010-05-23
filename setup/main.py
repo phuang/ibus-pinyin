83e194de430e70ca46034016f2b172e5ac8a2aeb
BYVoid <byvoid1@gmail.com>
2010-05-23 17:04:49 +0800

    Reorganize setup User Interface

--- setup/main.py
+++ setup/main.py
@@ -1,8 +1,10 @@
 # vim:set et ts=4 sts=4:
+# -*- coding: utf-8 -*-
 #
 # ibus-pinyin - The Chinese PinYin engine for IBus
 #
 # Copyright (c) 2008-2010 Peng Huang <shawn.p.huang@gmail.com>
+# Copyright (c) 2010 BYVoid <byvoid1@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+import sys
 import gtk
 import ibus
 import locale
@@ -28,7 +31,7 @@ from xdg import BaseDirectory
 _ = lambda a : gettext.dgettext("ibus-pinyin", a)
 
 class PreferencesDialog:
-    def __init__(self):
+    def __init__(self,engine):
         locale.setlocale(locale.LC_ALL, "")
         localedir = os.getenv("IBUS_LOCALEDIR")
         gettext.bindtextdomain("ibus-pinyin", localedir)
@@ -40,17 +43,94 @@ class PreferencesDialog:
         self.__builder.set_translation_domain("ibus-pinyin")
         self.__builder.add_from_file("ibus-pinyin-preferences.ui")
         self.__dialog = self.__builder.get_object("dialog")
+        self.__init_pages()
+        
+        if engine == "pinyin":
+            self.__config_namespace = "engine/Pinyin"
+            self.__init_general()
+            self.__init_pinyin()
+            self.__init_fuzzy()
+            self.__init_dictionary()
+            self.__init_about()
+        elif engine == "bopomofo":
+            self.__config_namespace = "engine/Bopomofo"
+            self.__init_general()
+            self.__init_bopomofo()
+            self.__init_fuzzy()
+            self.__init_dictionary()
+            self.__init_about()
+            self.__convert_fuzzy_pinyin_to_bopomofo()
+            
+        else:
+            print("Error: Unknown Engine")
+            exit()
+        
+        self.__pages.set_current_page(0)
 
-        self.__init_pinyin()
-        self.__init_init_state()
-        self.__init_others()
-        self.__init_ui()
-        self.__init_correct_pinyin()
-        self.__init_fuzzy_pinyin()
-        self.__init_dictionary()
-        self.__init_about()
+    def __init_pages(self):
+        self.__pages = self.__builder.get_object("pages")
+        self.__page_general = self.__builder.get_object("pageGeneral")
+        self.__page_pinyin_mode = self.__builder.get_object("pagePinyinMode")
+        self.__page_bopomofo_mode = self.__builder.get_object("pageBopomofoMode")
+        self.__page_fuzzy = self.__builder.get_object("pageFuzzy")
+        self.__page_dictionary = self.__builder.get_object("pageDictionary")
+        self.__page_about = self.__builder.get_object("pageAbout")
+        
+        self.__page_general.hide()
+        self.__page_pinyin_mode.hide()
+        self.__page_bopomofo_mode.hide()
+        self.__page_fuzzy.hide()
+        self.__page_dictionary.hide()
+        self.__page_about.hide()
+
+    def __init_general(self):
+        # page General
+        self.__page_general.show()
+        
+        # init state
+        self.__init_chinese = self.__builder.get_object("InitChinese")
+        self.__init_english = self.__builder.get_object("InitEnglish")
+        self.__init_full = self.__builder.get_object("InitFull")
+        self.__init_half = self.__builder.get_object("InitHalf")
+        self.__init_full_punct = self.__builder.get_object("InitFullPunct")
+        self.__init_half_punct = self.__builder.get_object("InitHalfPunct")
+        self.__init_simp = self.__builder.get_object("InitSimplifiedChinese")
+        self.__init_trad = self.__builder.get_object("IniTraditionalChinese")
+        
+        # UI
+        self.__lookup_table_page_size = self.__builder.get_object("LookupTablePageSize")
+        self.__lookup_table_orientation = self.__builder.get_object("LookupTableOrientation")
+        renderer = gtk.CellRendererText()
+        self.__lookup_table_orientation.pack_start(renderer)
+        self.__lookup_table_orientation.set_attributes(renderer, text=0)
+
+        # read values
+        self.__init_chinese.set_active(self.__get_value("InitChinese", True))
+        self.__init_full.set_active(self.__get_value("InitFull", False))
+        self.__init_full_punct.set_active(self.__get_value("InitFullPunct", True))
+        self.__init_simp.set_active(self.__get_value("InitSimplifiedChinese", True))
+        self.__lookup_table_orientation.set_active(self.__get_value("LookupTableOrientation", 0))
+        self.__lookup_table_page_size.set_value(self.__get_value("LookupTablePageSize", 5))
+
+        # connect signals
+        self.__init_chinese.connect("toggled", self.__toggled_cb, "InitChinese")
+        self.__init_full.connect("toggled", self.__toggled_cb, "InitFull")
+        self.__init_full_punct.connect("toggled", self.__toggled_cb, "InitFullPunct")
+        self.__init_simp.connect("toggled", self.__toggled_cb, "InitSimplifiedChinese")
+
+        def __lookup_table_page_size_changed_cb(adjustment):
+            self.__set_value("LookupTablePageSize", int(adjustment.get_value()))
+
+        def __lookup_table_orientation_changed_cb(widget):
+            self.__set_value("LookupTableOrientation", widget.get_active())
+
+        self.__lookup_table_orientation.connect("changed", __lookup_table_orientation_changed_cb)
+        self.__lookup_table_page_size.connect("value-changed", __lookup_table_page_size_changed_cb)
 
     def __init_pinyin(self):
+        # page
+        self.__page_pinyin_mode.show()
+        
         # pinyin
         self.__full_pinyin = self.__builder.get_object("FullPinyin")
         self.__incomplete_pinyin = self.__builder.get_object("IncompletePinyin")
@@ -71,15 +151,12 @@ class PreferencesDialog:
             # self.__incomplete_pinyin.set_sensitive(True)
             self.__double_pinyin_schema.set_sensitive(False)
             self.__double_pinyin_schema_label.set_sensitive(False)
+            self.__double_pinyin_show_raw.set_sensitive(False)
         else:
             # self.__incomplete_pinyin.set_sensitive(False)
             self.__double_pinyin_schema.set_sensitive(True)
             self.__double_pinyin_schema_label.set_sensitive(True)
-
-        def __full_pinyin_toggled_cb(widget):
-            val = widget.get_active()
-            self.__set_value("DoublePinyin", not val)
-            self.__incomplete_pinyin.set_sensitive(val)
+            self.__double_pinyin_show_raw.set_sensitive(True)
 
         def __double_pinyin_toggled_cb(widget):
             val = widget.get_active()
@@ -92,36 +169,37 @@ class PreferencesDialog:
             self.__set_value("DoublePinyinSchema", widget.get_active())
 
         # connect signals
-        # self.__full_pinyin.connect("toggled", __full_pinyin_toggled_cb)
         self.__double_pinyin.connect("toggled", __double_pinyin_toggled_cb)
         self.__incomplete_pinyin.connect("toggled", self.__toggled_cb, "IncompletePinyin")
         self.__double_pinyin_schema.connect("changed", __double_pinyin_schema_changed_cb)
         self.__double_pinyin_show_raw.connect("toggled", self.__toggled_cb, "DoublePinyinShowRaw")
-
-    def __init_init_state(self):
-        # init state
-        self.__init_chinese = self.__builder.get_object("InitChinese")
-        self.__init_english = self.__builder.get_object("InitEnglish")
-        self.__init_full = self.__builder.get_object("InitFull")
-        self.__init_half = self.__builder.get_object("InitHalf")
-        self.__init_full_punct = self.__builder.get_object("InitFullPunct")
-        self.__init_half_punct = self.__builder.get_object("InitHalfPunct")
-        self.__init_simp = self.__builder.get_object("InitSimplifiedChinese")
-        self.__init_trad = self.__builder.get_object("IniTraditionalChinese")
-
-        # read values
-        self.__init_chinese.set_active(self.__get_value("InitChinese", True))
-        self.__init_full.set_active(self.__get_value("InitFull", False))
-        self.__init_full_punct.set_active(self.__get_value("InitFullPunct", True))
-        self.__init_simp.set_active(self.__get_value("InitSimplifiedChinese", True))
+        
+        self.__init_input_custom()
+        self.__init_correct_pinyin()
+        
+    def __init_bopomofo(self):
+        # page Bopomodo Mode
+        self.__page_bopomofo_mode.show()
+        
+        # bopomofo
+        self.__bopomofo_keyboard_mapping = self.__builder.get_object("BopomofoKeyboardMapping")
+        self.__incomplete_bopomofo = self.__builder.get_object("IncompleteBopomofo")
+        renderer = gtk.CellRendererText()
+        self.__bopomofo_keyboard_mapping.pack_start(renderer)
+        self.__bopomofo_keyboard_mapping.set_attributes(renderer, text=0)
+        
+        # read value
+        self.__bopomofo_keyboard_mapping.set_active(self.__get_value("BopomofoKeyboardMapping", 0))
+        self.__incomplete_bopomofo.set_active(self.__get_value("IncompleteBopomofo", False))
 
         # connect signals
-        self.__init_chinese.connect("toggled", self.__toggled_cb, "InitChinese")
-        self.__init_full.connect("toggled", self.__toggled_cb, "InitFull")
-        self.__init_full_punct.connect("toggled", self.__toggled_cb, "InitFullPunct")
-        self.__init_simp.connect("toggled", self.__toggled_cb, "InitSimplifiedChinese")
+        def __bopomofo_keyboard_mapping_changed_cb(widget):
+            self.__set_value("BopomofoKeyboardMapping", widget.get_active())
+        
+        self.__bopomofo_keyboard_mapping.connect("changed", __bopomofo_keyboard_mapping_changed_cb)
+        self.__incomplete_bopomofo.connect("toggled", self.__toggled_cb, "IncompleteBopomofo")
 
-    def __init_others(self):
+    def __init_input_custom(self):
         # others
         self.__shift_select_candidate = self.__builder.get_object("ShiftSelectCandidate")
         self.__minus_equal_page = self.__builder.get_object("MinusEqualPage")
@@ -139,32 +217,6 @@ class PreferencesDialog:
         self.__minus_equal_page.connect("toggled", self.__toggled_cb, "MinusEqualPage")
         self.__comma_period_page.connect("toggled", self.__toggled_cb, "CommaPeriodPage")
         self.__auto_commit.connect("toggled", self.__toggled_cb, "AutoCommit")
-
-    def __init_ui(self):
-        # UI
-        self.__lookup_table_page_size = self.__builder.get_object("LookupTablePageSize")
-        self.__lookup_table_orientation = self.__builder.get_object("LookupTableOrientation")
-        self.__trad_candidate = self.__builder.get_object("TradCandidate")
-
-        renderer = gtk.CellRendererText()
-        self.__lookup_table_orientation.pack_start(renderer)
-        self.__lookup_table_orientation.set_attributes(renderer, text=0)
-
-        # read values
-        self.__lookup_table_orientation.set_active(self.__get_value("LookupTableOrientation", 0))
-        self.__lookup_table_page_size.set_value(self.__get_value("LookupTablePageSize", 5))
-        self.__trad_candidate.set_active(self.__get_value("TradCandidate", False))
-
-        # connect signals
-        def __lookup_table_page_size_changed_cb(adjustment):
-            self.__set_value("LookupTablePageSize", int(adjustment.get_value()))
-
-        def __lookup_table_orientation_changed_cb(widget):
-            self.__set_value("LookupTableOrientation", widget.get_active())
-
-        self.__lookup_table_orientation.connect("changed", __lookup_table_orientation_changed_cb)
-        self.__lookup_table_page_size.connect("value-changed", __lookup_table_page_size_changed_cb)
-        self.__trad_candidate.connect("toggled", self.__toggled_cb, "TradCandidate")
 
     def __init_correct_pinyin(self):
         # auto correct
@@ -196,7 +248,10 @@ class PreferencesDialog:
             widget = self.__builder.get_object(name)
             widget.connect("toggled", self.__toggled_cb, name)
 
-    def __init_fuzzy_pinyin(self):
+    def __init_fuzzy(self):
+        # page Fuzzy
+        self.__page_fuzzy.show()
+        
         # fuzzy pinyin
         self.__fuzzy_pinyin = self.__builder.get_object("FuzzyPinyin")
         self.__fuzzy_pinyin_widgets = [
@@ -243,7 +298,36 @@ class PreferencesDialog:
             widget = self.__builder.get_object(name)
             widget.connect("toggled", self.__toggled_cb, name)
 
+    def __convert_fuzzy_pinyin_to_bopomofo(self):
+        self.__builder.get_object("FuzzyPinyin_C_CH").set_label("ㄘ => ㄔ")
+        self.__builder.get_object("FuzzyPinyin_Z_ZH").set_label("ㄗ => ㄓ")
+        self.__builder.get_object("FuzzyPinyin_S_SH").set_label("ㄙ => ㄕ")
+        self.__builder.get_object("FuzzyPinyin_CH_C").set_label("ㄔ => ㄘ")
+        self.__builder.get_object("FuzzyPinyin_ZH_Z").set_label("ㄓ => ㄗ")
+        self.__builder.get_object("FuzzyPinyin_SH_S").set_label("ㄕ => ㄙ")
+        self.__builder.get_object("FuzzyPinyin_L_N").set_label("ㄌ => ㄋ")
+        self.__builder.get_object("FuzzyPinyin_F_H").set_label("ㄈ => ㄏ")
+        self.__builder.get_object("FuzzyPinyin_L_R").set_label("ㄌ => ㄖ")
+        self.__builder.get_object("FuzzyPinyin_K_G").set_label("ㄎ => ㄍ")
+        self.__builder.get_object("FuzzyPinyin_N_L").set_label("ㄋ => ㄌ")
+        self.__builder.get_object("FuzzyPinyin_H_F").set_label("ㄏ => ㄈ")
+        self.__builder.get_object("FuzzyPinyin_R_L").set_label("ㄖ => ㄌ")
+        self.__builder.get_object("FuzzyPinyin_G_K").set_label("ㄍ => ㄎ")
+        self.__builder.get_object("FuzzyPinyin_AN_ANG").set_label("ㄢ => ㄤ")
+        self.__builder.get_object("FuzzyPinyin_EN_ENG").set_label("ㄣ => ㄥ")
+        self.__builder.get_object("FuzzyPinyin_IN_ING").set_label("ㄧㄣ => ㄧㄥ")
+        self.__builder.get_object("FuzzyPinyin_ANG_AN").set_label("ㄤ => ㄢ")
+        self.__builder.get_object("FuzzyPinyin_ENG_EN").set_label("ㄥ => ㄣ")
+        self.__builder.get_object("FuzzyPinyin_ING_IN").set_label("ㄧㄥ => ㄧㄣ")
+        self.__builder.get_object("FuzzyPinyin_IAN_IANG").set_label("ㄧㄢ => ㄧㄤ")
+        self.__builder.get_object("FuzzyPinyin_UAN_UANG").set_label("ㄨㄢ => ㄨㄤ")
+        self.__builder.get_object("FuzzyPinyin_IANG_IAN").set_label("ㄧㄤ => ㄧㄢ")
+        self.__builder.get_object("FuzzyPinyin_UANG_UAN").set_label("ㄨㄤ => ㄨㄢ")
+
     def __init_dictionary(self):
+        # page Dictionary
+        self.__page_dictionary.show()
+    
         # dictionary
         self.__special_phrases = self.__builder.get_object("SpecialPhrases")
         self.__edit_special_phrases = self.__builder.get_object("EditSpecialPhrases")
@@ -268,6 +352,9 @@ class PreferencesDialog:
         self.__edit_special_phrases.connect("clicked", __edit_special_phrases_clicked_cb)
 
     def __init_about(self):
+        # page About
+        self.__page_about.show()
+        
         self.__name_version = self.__builder.get_object("NameVersion")
         self.__name_version.set_markup(_("<big><b>IBus Pinyin %s</b></big>") % version.get_version())
 
@@ -278,20 +365,20 @@ class PreferencesDialog:
         self.__set_value(name, widget.get_active ())
 
     def __get_value(self, name, defval):
-        value = self.__config.get_value("engine/Pinyin", name, "test_default_value_9898")
+        value = self.__config.get_value(self.__config_namespace, name, "test_default_value_9898")
         if value != "test_default_value_9898":
             return value
         self.__set_value(name, defval)
         return defval
 
     def __set_value(self, name, val):
-        self.__config.set_value("engine/Pinyin", name, val)
+        self.__config.set_value(self.__config_namespace, name, val)
 
     def run(self):
         return self.__dialog.run()
 
 def main():
-    PreferencesDialog().run()
+    PreferencesDialog(sys.argv[1]).run()
 
 
 if __name__ == "__main__":
